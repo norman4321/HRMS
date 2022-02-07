b5b43e9e8d45540f0de088089cdc665578a04d12
norman4321 <normancabiling123@gmail.com>
2022-02-07 21:41:11 +0800

    Status and Role (User Management)

--- config/user_view.php
+++ config/user_view.php
@@ -3,7 +3,7 @@
 function UserList(){
     include "../../config/database.php";
 
-    $sql = "SELECT * FROM hrms_user_profile";
+    $sql = "SELECT * FROM HRMS_user_profile INNER JOIN HRMS_user_account ON user_id=profile_id INNER JOIN HRMS_user_type ON user_type=role_id INNER JOIN HRMS_user_status ON user_status=status_id";
     $result = mysqli_query($conn,$sql);
 
     while($row = mysqli_fetch_array($result))
@@ -12,8 +12,8 @@ function UserList(){
     echo "<td>" . $row['profile_id'] . "</td>";
     echo "<td>" . $row['profile_firstname'] . "</td>";
     echo "<td>" . $row['profile_lastname'] . "</td>";
-    echo "<td>" . "Admin" . "</td>";
-    echo "<td>". "<span class='status Active'>Active</span>" . "</td>";
+    echo "<td>" . $row['role_description'] . "</td>";
+    echo "<td>" ."<span class='status $row[status_description]'>". $row['status_description']."</span>"."</td>";
     echo "<td>". "<button class='btn btn-success'><a href='edit_user.php?id=$row[profile_id]' class='text-light'>Edit</a></button>" ."</td>";
     echo "</tr>";
     }
